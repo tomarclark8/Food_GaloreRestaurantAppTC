264bf47a76125a8c55013c8997b3cb5d28261809
Tomar Clark <ttclark.create@gmail.com>
2025-10-21 10:34:37 -0400

    This is the setup of my restaurant app and my first commit

---
+++ app/src/main/java/edu/bpi/myrestaurant/MainActivitythree.java
@@ -0,0 +1,22 @@
+package edu.bpi.myrestaurant;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivitythree extends AppCompatActivity {
+
+    //this is the menu page
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main_activitythree);
+
+    }
+}
