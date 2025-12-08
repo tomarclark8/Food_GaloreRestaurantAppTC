bffc131674b15d4ffc5f40af47d25247552b777a
Tomar Clark <ttclark.create@gmail.com>
2025-12-08 10:09:06 -0500

    Buttons for orders placed

--- app/src/main/java/edu/bpi/myrestaurant/Menu.java
+++ app/src/main/java/edu/bpi/myrestaurant/Menu.java
@@ -13,22 +13,91 @@ public class Menu extends AppCompatActivity {
 
     //this is the menu page
     Button Back;
+    Button oneAdd;
+    Button twoAdd;
+    Button threeAdd;
+    Button fourAdd;
+    Button fiveAdd;
+    Button sixAdd;
 
+    Intent orders = new Intent();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_home);
+        setContentView(R.layout.activity_main_menu);
 
-        Back = (Button) findViewById(R.id.Back);
+        Back = (Button) findViewById(R.id.mback);
+        oneAdd = (Button) findViewById(R.id.add1);
+        twoAdd = (Button) findViewById(R.id.add2);
+        threeAdd = (Button) findViewById(R.id.add3);
+        fourAdd = (Button) findViewById(R.id.add4);
+        fiveAdd = (Button) findViewById(R.id.add5);
+        sixAdd = (Button) findViewById(R.id.add6);
+
 
         Back.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v){
-                Intent intent = new Intent(Menu.this, Home.class);
-                startActivity(intent);
+                Intent back = new Intent(Menu.this, Home.class);
+                startActivity(back);
 
             }
         });
+        //Food 1
+        oneAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                orders = new Intent(Menu.this, OrdersPlaced.class);
+                startActivity(orders);
+
+            }
+        });
+        //Food 2
+        twoAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                orders = new Intent(Menu.this, OrdersPlaced.class);
+                startActivity(orders);
+
+            }
+        });
+        //Food 3
+        threeAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                orders = new Intent(Menu.this, OrdersPlaced.class);
+                startActivity(orders);
+
+            }
+        });
+        //Food 4
+        fourAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                orders = new Intent(Menu.this, OrdersPlaced.class);
+                startActivity(orders);
+
+            }
+        });
+        //Food 5
+        fiveAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                orders = new Intent(Menu.this, OrdersPlaced.class);
+                startActivity(orders);
+
+            }
+        });
+        //Food 6
+        sixAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                orders = new Intent(Menu.this, OrdersPlaced.class);
+                startActivity(orders);
+
+            }
+        });
+
     }
 }
--- app/src/main/java/edu/bpi/myrestaurant/Reservation.java
+++ app/src/main/java/edu/bpi/myrestaurant/Reservation.java
@@ -1,6 +1,9 @@
 package edu.bpi.myrestaurant;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,12 +11,22 @@ import androidx.appcompat.app.AppCompatActivity;
 public class Reservation extends AppCompatActivity {
 
     //this is the menu page
-
+    Button Back;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main_reservation);
 
+        Back = (Button) findViewById(R.id.rback);
+
+        Back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent(Reservation.this, Home.class);
+                startActivity(intent);
+
+            }
+        });
     }
 }
