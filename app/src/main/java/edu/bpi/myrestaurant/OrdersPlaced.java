ef6158a505cca728ea982d0e2eb51ffba0376ad3
Tomar Clark <ttclark.create@gmail.com>
2025-12-08 10:34:35 -0500

    OrdersPlaced ids Updated

--- app/src/main/java/edu/bpi/myrestaurant/OrdersPlaced.java
+++ app/src/main/java/edu/bpi/myrestaurant/OrdersPlaced.java
@@ -2,6 +2,7 @@ package edu.bpi.myrestaurant;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,12 +11,27 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class OrdersPlaced extends AppCompatActivity {
+    TextView f1;
+    TextView f2;
+    TextView f3;
+    TextView f4;
+    TextView f5;
+    TextView f6;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_orders_placed);
+
+        f1 = (TextView) findViewById(R.id.f1);
+        f2 = (TextView) findViewById(R.id.f2);
+        f3 = (TextView) findViewById(R.id.f3);
+        f4 = (TextView) findViewById(R.id.f4);
+        f5 = (TextView) findViewById(R.id.f5);
+        f6 = (TextView) findViewById(R.id.f6);
+
+
 
         Intent getOrders = new Intent();
         getOrders.getIntExtra("CP1", 1);
